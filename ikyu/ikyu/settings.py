6b45c024f33b633b15081dbe72587d0cbf13357e
Syed Towfiqur Rahim <5446400-aqifcse@users.noreply.gitlab.com>
2021-02-22 16:10:37 +0600

    Scrapy-splash added

--- ikyu/ikyu/settings.py
+++ ikyu/ikyu/settings.py
@@ -52,9 +52,9 @@ SPIDER_MIDDLEWARES = {
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'ikyu.middlewares.MyCustomDownloaderMiddleware': 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+   'ikyu.middlewares.MyCustomDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
@@ -70,21 +70,43 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+#AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
+#@AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 60
+#AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG = False
+#AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 0
-HTTPCACHE_DIR = 'httpcache'
-HTTPCACHE_IGNORE_HTTP_CODES = []
-HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+FEED_EXPORT_ENCODING = "utf-8"
+
+SPLASH_URL = 'http://localhost:8050'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
+CONCURRENT_REQUESTS = 1
+
--- ikyu/ikyu/spiders/ikyu_restaurant.py
+++ ikyu/ikyu/spiders/ikyu_restaurant.py
@@ -1,35 +1,156 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy_splash import SplashRequest
+from urllib.parse import urljoin
+
+LUA_SCRIPT = """
+function main(splash)
+    splash.private_mode_enabled = false
+    splash:go(splash.args.url)
+    splash:wait(2)
+    html = splash:html()
+    splash.private_mode_enabled = true
+    return html
+end
+"""
 
 
 class IkyuRestaurantSpider(scrapy.Spider):
     name = 'ikyu_restaurant'
-    allowed_domains = ['https://restaurant.ikyu.com/']
+    allowed_domains = ['restaurant.ikyu.com']
 
     start_urls = ['https://restaurant.ikyu.com/104864/?pups=2&pndt=1/']     # '/' must me included :(
     base_url = 'https://restaurant.ikyu.com/104864/?pups=2&pndt=1/'         # '/' must be included  :(
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(
+                url=url,
+                callback=self.parse,
+                endpoint='execute',
+                args={
+                    'wait': 1,
+                    "lua_source":LUA_SCRIPT
+                }
+            )
     
     def parse(self, response):
-        def extract_with_css(query):
-            return response.css(query).extract_first().strip()
 
-        all_items = response.xpath('//li[@class="_1QMibK-"]')
+        plans = response.xpath('//div[@id="plans"]/section/div[3]/ul')
 
-        for item in all_items:
+        for plan in plans:
 
-            meal_type               = extract_with_css('div.pYabCMk.span.font.font::text')
-            # title                   = extract_with_css()
-            # regular_fee             = extract_with_css()
-            # course_only             = extract_with_css()
-            # course_dessert          = extract_with_css()
-            # course_dessert_drink    = extract_with_css()
+            meal_type               = plan.xpath('.//li[@class="_1QMibK-"]/div/div/div/div/span/text()').extract_first().replace('\n', '').replace(' ', '')
+            title                   = plan.xpath('.//li[@class="_1QMibK-"]/div/div/div/h3/text()').extract_first().replace('\n', '').replace(' ', '')
+            course_type             = plan.xpath('.//li[@class="_1QMibK-"]/div/div/div/span[1]/text()').extract_first().replace('\n', '').replace(' ', '')
+            regular_fee             = plan.xpath('.//li[@class="_1QMibK-"]/div/div/div/span[2]/text()').extract_first().replace('\n', '').replace(' ', '')
 
-            yield {
-                'meal_type' :               meal_type, 
-                # 'title' :                   title, 
-                # 'regular_fee' :             regular_fee , 
-                # 'course_only' :             course_only, 
-                # 'course_dessert' :          course_dessert, 
-                # 'course_dessert_drink' :    course_dessert_drink, 
-            }
+            # offer_list = []
+            scrap_output = { 'meal_type' : meal_type, 'title' :  title, 'course_type' : course_type, 'regular_fee' : regular_fee, 'offers' : [] }
+
+            plan_offers = plan.xpath('.//li[@class="_1QMibK-"]/div/ul')
+
+            for plan_offer in plan_offers:
+                offer_title           = plan_offer.xpath('.//li[@class="_3S76qhi"]/a/div[1]/div[1]/h4/text()').extract_first()
+                offer_current_price   = plan_offer.xpath('.//li[@class="_3S76qhi"]/a/div[1]/div[2]/div[2]/span/span[2]/text()').extract_first()
+                offer_prev_price      = plan_offer.xpath('.//li[@class="_3S76qhi"]/a/div[1]/div[2]/div[1]/span/text()').extract_first()
+                offer_off             = plan_offer.xpath('.//li[@class="_3S76qhi"]/a/div[1]/div[2]/div[2]/span/span[1]/text()').extract_first()
+                offer = "title : " + str(offer_title) + "current_price : " +  str(offer_current_price) + "previous_price : " + str(offer_prev_price) + "off : " + str(offer_off)
+                scrap_output['offers'].append(offer)
         
+        yield scrap_output
+
+        
+
+
+
+
+# class PaypaySpider(scrapy.Spider):
+#     name = 'paypay'
+#     allowed_domains = ['paypaymall.yahoo.co.jp']
+#     start_urls = ['https://paypaymall.yahoo.co.jp/?sc_e=ytc/']
+
+#     
+
+#     def parse(self, response):
+
+#         top_rankings = []
+#         famous_stores = []
+
+#         scrap_output = { 
+#             'top_rankings': top_rankings, 
+#             'famous_stores': famous_stores 
+#         }
+
+#         # --------------------------------------------Top Ranking-------------------------------------------------------------------------------------------------
+        
+#         top_rankings = response.xpath('//body/div/main/div[@class = "Partition top_ranking Partition-separate"]/div[@class = "RankingCarousel"]/amp-list/div[@class="i-amphtml-fill-content i-amphtml-replaced-content"]/amp-carousel/div[@class="i-amphtml-scrollable-carousel-container"]').extract()
+
+#         ranking_categories = response.xpath('.//div[@class="RankingItem amp-carousel-slide amp-scrollable-carousel-slide"]/div')
+
+#         for category in ranking_categories:
+#             category_name = category.xpath('./p/a/text()').extract_first().replace(' ', '').replace('\n', '')
+#             items = category.xpath('.//div[@class="TinyShelfItem_info"]')
+
+#             ranking_item_list = []
+
+#             ranking_output = {
+#                 'category_name' : category_name,
+#                 'items' : ranking_item_list
+#             }
+        
+#             for item in items:
+#                 brand = item.xpath('./p[@class="TinyShelfItem_brand"]/text()').extract_first()
+#                 name = item.xpath('./p[@class="TinyShelfItem_name"]/text()').extract_first()
+#                 price = item.xpath('./p[@class="TinyShelfItem_price"]/text()').extract_first()
+#                 # yield {
+#                 #     'category_name': category_name,
+#                 #     'item_brand' : brand,
+#                 #     'item_name' : name,
+#                 #     'item_price' : price,
+#                 # }
+
+#                 ranking_items_out = {
+#                     'item_brand' : brand,
+#                     'item_name' : name,
+#                     'item_price' : price,
+#                 }
+
+#                 ranking_output['items'].append(ranking_items_out)
+#             scrap_output['top_rankings'].append(ranking_output) 
+
+#         #------------------------------------------Famous Store ----------------------------------------------------------------------------------------------------------
+#         famous_stores = response.xpath('//body/div/main/div[@class = "Partition top_famousStore Partition-separate"]/div[@class = "RecommendStoreCarousel"]/amp-list/div[@class="i-amphtml-fill-content i-amphtml-replaced-content"]/amp-carousel/div[@class="i-amphtml-scrollable-carousel-container"]').extract()
+
+#         stores = response.xpath('.//div[@class="RecommendStoreItem amp-carousel-slide amp-scrollable-carousel-slide"]/div')
+
+#         for store in stores:
+#             store_name = store.xpath('./p/a/text()').extract_first().replace(' ', '').replace('\n', '')
+#             items = store.xpath('.//div[@class="RecommendStoreItem_unit"]/div/a/div')
+
+#             store_item_list = []
+
+#             store_output = {
+#                 'store_name' : store_name,
+#                 'items' : store_item_list
+#             }
+        
+#             for item in items:
+#                 item_title = item.xpath('./p[@class="TinyItem_title"]/text()').extract_first()
+#                 price = item.xpath('./p[@class="TinyItem_price"]/text()').extract_first()
+
+#                 # yield {
+#                 #     'store_name': store_name,
+#                 #     'item_title' : item_title,
+#                 #     'item_price' : price,
+#                 # }
+
+#                 store_items_out = {
+#                     'item_title' : item_title,
+#                     'item_price' : price,
+#                 }
+
+#                 store_output['items'].append(store_items_out)
+#             scrap_output['famous_stores'].append(store_output)
+
+#         yield scrap_output
